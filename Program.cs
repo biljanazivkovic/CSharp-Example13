d731405df8e9e6fd825386f85b401d11adf91c5d
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:24:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ex13
+{
+    /*
+    class Car
+    {
+        public string model;
+
+        public Car()
+        {
+            model = "Mustang";
+        }
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();
+            Console.WriteLine(Ford.model);
+        }
+    }
+    */
+    class Car
+    {
+        public string model;
+        public string color;
+        public int year;
+
+        public Car(string modelName, string modelColor, int modelYear)
+        {
+            model = modelName;
+            color = modelColor;
+            year = modelYear;
+        }
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "red", 1969);
+            Car Opel = new Car("Astra", "white", 2005);
+            Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+            Console.WriteLine(Opel.color + " " + Opel.year + " " + Opel.model);
+        }
+    }
+}
